53daaaf5d1f1fd5d86d00a8bb98ae0b258651027
gca0011 <gca0011@alu.ubu.es>
2018-11-26 21:15:06 +0100

    Primera implementación para añadir tarea.

--- ProyectoDMS/src/Backlog.java
+++ ProyectoDMS/src/Backlog.java
@@ -1,11 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public abstract class Backlog {
+	private static String[] estados = {"Pendiente","En_proceso","Validacion","Terminado"};
 	protected HashSet<Tarea> tarea= new HashSet<Tarea>();
+	
 	/**
+	 *
 	 * Aqui añadiriamos la tarea, pues en cuanto se crea una tarea va a ir a un 
 	 * tipo de baclog, con eso crearimos los metodos y pasariamos los atributos de cada backlog
 	 * @param id
 	 */
-	public abstract void addTarea(int id);
+	public abstract void addTarea(Tarea t);
 }
